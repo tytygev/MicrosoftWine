fedb8d4c9c20f1b84e37a24c33d1fc3d98b74817
tytygev <photodc@ya.ru>
2025-05-05 22:26:47 +0300

    переезд

--- MicrosoftWine/My Project/Application.Designer.vb
+++ MicrosoftWine/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.MicrosoftWine.Form1
+            Me.MainForm = Global.MicrosoftWine_vb.Form1
         End Sub
     End Class
 End Namespace
