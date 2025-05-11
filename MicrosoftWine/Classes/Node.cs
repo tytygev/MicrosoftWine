981405f887aa75399cc207b18f52af11b46a1aa2
tytygev <photodc@ya.ru>
2025-05-11 17:36:57 +0300

    Классы граф, линк, нода, контроллер, плеерстейт.
    Перенос в отдельные файлы

---
+++ MicrosoftWine/Classes/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftWine.Classes
+{
+    public class Node
+    {
+        public int Id;
+
+        public Node(int id)
+        {
+            Id = id;
+        }
+    }
+}
