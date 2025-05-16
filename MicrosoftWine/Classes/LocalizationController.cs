b2d4df04e6a85bb35c5cd0098098a28cde0b3c7b
tytygev <photodc@ya.ru>
2025-05-16 17:13:20 +0300

    Черновой билдер локализации

--- MicrosoftWine/Classes/LocalizationController.cs
+++ MicrosoftWine/Classes/LocalizationController.cs
@@ -1,24 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 namespace MicrosoftWine
 {
 	class LocalizationController
 	{
-		Dictionary<int, string> LocDic = new Dictionary<int, string>();
-		string localData = "local.txt";
+		public Dictionary<string, string> LocalizationDictionary;		
+		
 
 
-		public void LocalizationFileParcer(string path)
+		public Dictionary<string, string> BuildLocalization()
 		{
+			const string LocalizationFile = "local.txt";
+			const Char SeparatorChar = ';';
+
+			Dictionary<string, string> locDic=new Dictionary<string, string>();
+
+			StringReader file = new StringReader(LocalizationFile);
 
 			string line;
-			StringReader file = new StringReader(path);
-
 			while ((line = file.ReadLine()) != null)
 			{
-
-			}
-		}
+				string[] pairs = line.Split(SeparatorChar);
+				string key = pairs[0];
+				string value = pairs[1];
+                locDic.Add(key, value);
+            }
+			return locDic;
+        }
 	}
 }
